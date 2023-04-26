44701f916cb3658020b49c249c67d328eec1d9b7
Bertrand Keller <bertrand.keller@gmail.com>
2023-04-26 17:05:50 +0200

    fix: theme

--- .app/.eleventy.js
+++ .app/.eleventy.js
@@ -4,6 +4,7 @@ module.exports = (eleventyConfig) => {
   // Load eleventy configurations from './_config' folder
   loader([__dirname, "_config"], eleventyConfig);
 
+  eleventyConfig.addPassthroughCopy("./../assets");
   return {
     pathPrefix: process.env.ELEVENTY_NOTES_PATH_PREFIX || undefined,
     dir: {
